3e9080883f38b8f6699b32ec1005f7d5aa9fe083
QiminCao <sevenscao@gmail.com>
2019-04-19 10:52:10 -0700

    initial

---
+++ imports/ui/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+
+class App extends React.Component {
+	render() {
+		return (
+			<Container>
+				<h1>Welcome to wiki viewer</h1>
+			</Container>
+		);
+	}
+
+}
+
+export default App;
